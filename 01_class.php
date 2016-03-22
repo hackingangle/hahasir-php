5ae7b40f576f6874a462386f94cbfaf7b0f010f2
hackingangle <hackingangle@gmail.com>
2016-03-22 09:42:21 +0800

    first commit

---
+++ 01_class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 面向对象-类
+ * Class
+ * Mehtod
+ * Property
+ * Object
+ * PSR
+ */
+class Person
+{
+    public $age = 0;
+
+    /**
+     * 长大一岁
+     * @return null
+     */
+    public function growUp() 
+    {
+        $this->age++;
+    }
+}
+
+$hahaSir = new Person();
+$hahaSir->growUp();
+$hahaSir->growUp();
+$hahaSir->growUp();
+var_dump($hahaSir);
+$zhangsan = new Person();
+$zhangsan->growUp();
+var_dump($zhangsan);
